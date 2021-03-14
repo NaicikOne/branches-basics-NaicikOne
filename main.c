48f941913b4f82fb69cbb4ab84e9fc7acf174dac
NaicikOne <NaicikOne@yandex.ru>
2021-03-14 20:08:46 +0600

    add multiplication

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 
 int main() {
   printf("Hello! I am calcu!\n");
-  int a = 2, b = 2, c;
+  int a = 3, b = 2, c, d;
   c = a + b;
-  a = a - b;
   printf("summ = %d \n", c);
-  printf("diff = %d \n", a);
+  c = 0;
+  d = a - b;
+  printf("diff = %d \n", d);
+  c = a * b;
+  printf("mult = %d \n", c);
   system("pause");
   return 0;
 }
