c93ed8280bb50a4469573648fe3c10d07bdca25b
NaicikOne <NaicikOne@yandex.ru>
2021-03-14 20:00:43 +0600

    Add diff

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-  printf("WOW! i am alive!\n");
-  int a = 2, b = 2;
-  a = a + b;
-  printf("summ = %d \n", a);
+  printf("Hello! I am calcu!\n");
+  int a = 2, b = 2, c;
+  c = a + b;
+  a = a - b;
+  printf("summ = %d \n", c);
+  printf("diff = %d \n", a);
   system("pause");
   return 0;
 }
